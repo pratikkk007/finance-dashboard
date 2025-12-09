522fc218d9ccb281c276e50a64e7daa26370fcd3
Pratik Wani <wanipratik30@gmail.com>
2025-12-09 20:14:09 +0530

    first commit

---
+++ Java-DSA/hello.java
@@ -0,0 +1,11 @@
+class Hello
+{
+    public static void main(String[] args)
+    {
+        int num1 = 3;
+        int num2 = 4;
+        int result = num1 + num2;
+        System.out.println(result);
+        
+    }
+}
