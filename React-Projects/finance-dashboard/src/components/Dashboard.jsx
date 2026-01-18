e8d747a11ac79ce080f5c81b4ff6af555fef831e
Pratik Wani <wanipratik30@gmail.com>
2026-01-18 12:03:03 +0530

    Day 4: useEffect refractored

--- React-Projects/finance-dashboard/src/components/Dashboard.jsx
+++ React-Projects/finance-dashboard/src/components/Dashboard.jsx
@@ -1,35 +1,40 @@
-import ExpenseList from "./ExpenseList";
 import { useState, useEffect } from "react";
 import AddExpense from "./AddExpense";
+import ExpenseList from "./ExpenseList";
 
 function Dashboard() {
+  // 1. State
   const [expenses, setExpenses] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
+  // 2. Effects
   useEffect(() => {
     setTimeout(() => {
       setExpenses([
         { id: 1, title: "Food", amount: 250 },
-        { id: 2, title: "Hotel", amount: 700 },
+        { id: 2, title: "Travel", amount: 120 },
       ]);
       setIsLoading(false);
     }, 1000);
   }, []);
 
+  // 3. Handlers
   function addExpenseHandler(expense) {
-    setExpenses((prevExpenses) => {
-      return [expense, ...prevExpenses];
-    });
-  }
-  if (isLoading) {
-    return <p>Loading...</p>;
+    setExpenses((prevExpenses) => [expense, ...prevExpenses]);
   }
 
+  // 4. Conditional UI
+  if (isLoading) {
+    return <p>Loading expenses...</p>;
+  }
+
+  // 5. JSX
   return (
     <div>
       <h2>
         {expenses.length > 0 ? "Your Expenses" : "Start Tracking Expenses"}
       </h2>
+
       <AddExpense onAddExpense={addExpenseHandler} />
       <ExpenseList expenses={expenses} />
     </div>
--- React-Projects/finance-dashboard/src/components/Header.jsx
+++
@@ -1,5 +0,0 @@
-function Header() {
-  return <h1>Personal Finance Dashboard</h1>
-}
-
-export default Header;
